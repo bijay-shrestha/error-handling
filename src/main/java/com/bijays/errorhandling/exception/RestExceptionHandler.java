5d046c0e9b89069a325bbcd25c8b207a72dd9c01
Bijay Shrestha <bijay.shrestha@f1soft.com>
2019-08-12 00:49:04 +0545

    Remove @ExceptionHandler values from RestExceptionHandler.

--- src/main/java/com/bijays/errorhandling/exception/RestExceptionHandler.java
+++ src/main/java/com/bijays/errorhandling/exception/RestExceptionHandler.java
@@ -4,8 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 /**
@@ -15,24 +15,24 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
  * @version $Id: $Id
  */
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value = {DataNotFoundException.class})
+    @ExceptionHandler
     public ResponseEntity<Object> handleDataNotFoundException(DataNotFoundException ex){
         log.error("-------- DATA NOT FOUND EXCEPTION ------");
         return buildResponseEntity(ex.getException());
     }
 
-    @ExceptionHandler(value = {OperationUnSuccessfulException.class})
+    @ExceptionHandler
     public ResponseEntity<Object> handleOperationUnsuccessfulException(OperationUnSuccessfulException ex){
         log.error("------- OPERATION UNSUCCESSFUL EXCEPTION -------");
         return buildResponseEntity(ex.getException());
     }
 
-    @ExceptionHandler(value = {OrderServiceException.class})
+    @ExceptionHandler
     public ResponseEntity<Object> handleOrderServiceException(OrderServiceException ex){
         log.error("------- ORDER SERVICE EXCEPTION -------");
         return buildResponseEntity(ex.getException());
